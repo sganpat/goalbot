8ac203abfe424a2117348283673933f0efbb01b9
sganpat <sganpat@gmail.com>
2021-04-28 19:30:20 -0400

    added validation to custom actions and modified rules and stories.

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.types import DomainDict
 
 
 class ValidateGoalForm(FormValidationAction):
@@ -29,10 +30,75 @@ class ValidateGoalForm(FormValidationAction):
     #     else:
     #         return {"exercise": "None", "confirm_exercise": False }
 
+    def validate_specific_goal(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        """Validate specific goal"""
+
+        # Check if specific goal is no and ask them to restate the goal
+        if value:
+            return {"specific_goal": True}
+        else:
+            dispatcher.utter_message(response="utter_make_specific_goal")
+            return {"goal": None, "specific_goal": None}
+    
+    def validate_challenging_goal(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        """Validate challenging goal"""
+
+        # Check if challenging goal is no and ask them to restate the goal
+        if value:
+            return {"challenging_goal": True}
+        else:
+            dispatcher.utter_message(response="utter_make_challenging_goal")
+            return {"goal": None, "challenging_goal": None}
+    
+    def validate_persistent_goal(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        """Validate persistent goal"""
+
+        # Check if persistent goal is no
+        if value:
+            return {"persistent_goal": True}
+        else:
+            dispatcher.utter_message(response="utter_make_persistent_goal")
+            return {"persistent_goal": False}
+    
+    def validate_goal_actions(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        """Validate goal actions"""
+
+        return []
+    
+
 
 class ActionSubmitResults(Action):
     def name(self) -> Text:
         return "action_submit_results"
+
     def run(
         self,
         dispatcher: CollectingDispatcher,
